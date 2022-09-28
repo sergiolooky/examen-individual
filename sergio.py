cebbacc30f2dc32f253395bc2651868900c66f51
sergiolooky <111609774+sergiolooky@users.noreply.github.com>
2022-09-28 12:50:43 -0400

    Add files via upload

---
+++ sergio.py
@@ -0,0 +1,10 @@
+numero =int(input("ingrese un numero"))
+i=2
+total=0
+while i<numero
+     if numero%i==0
+       print("no es primo")
+        i=i+1
+         print("es primo")
+                return total
+        
